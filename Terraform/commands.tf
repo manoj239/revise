9d01c8351b8e36015d3109ab8d16a64f4d946d4d
Manoj Kumar Borra <manojkumar.borra@infosys.com>
2025-08-07 11:38:23 -0400

    Updated

---
+++ Terraform/commands.tf
@@ -0,0 +1,8 @@
+terraform individually can't do anything, it uses plugins to build infrstructure. When we give terraform init 
+all the plugins like .terraform etc files will get installed.
+
+terraform init, terraform fmt,validate, plan, apply
+terraform state list
+terraform state show <resorce name>
+
+
